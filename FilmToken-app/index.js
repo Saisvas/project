aeb1a130fac56a732e1ead11569d2ad08adb7f9c
Sai_svas_G <saisvasgudimalla@gmail.com>
2023-05-09 14:51:50 -0400

    Update index.js

--- FilmToken-app/index.js
+++ FilmToken-app/index.js
@@ -7,6 +7,6 @@ app.get('/', function (req, res) {
     res.render('index.html');
 });
 
-app.listen(443, function () {
+app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
